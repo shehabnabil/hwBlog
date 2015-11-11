71fe063e801f1e9123db83b06c15376111dbb985
Joe Spampinato <jcspampinato@gmail.com>
2015-11-11 17:06:40 -0500

    fixes to verify new,create,destroy session works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+	def create
+		user = User.new(user_params.dup)
+		if (user.save) 
+			flash[:notice] = "Your account was created successfully"
+			redirect_to users_path
+		else
+			errmsg = "There was a problem saving your account"
+			if user.errors.full_messages.any?
+				user.errors.full_messages.each do |error_message|
+               errmsg += "<br>" + error_message
+				end
+			end
+			flash[:alert] = errmsg.html_safe
+			@user = user
+			render :new
+		end
+	end
+	def destroy
+		user = User.find_by_id params[:id]
+
+		if user.destroy
+		 flash[:notice] = "User deleted successfully."
+		else
+		 flash[:alert] = "There was a problem deleting the user."
+		end
+		redirect_to users_path
+	end
 	def index
 		@users = User.all
 	end
@@ -8,5 +35,10 @@ class UsersController < ApplicationController
 	def show
 		user =User.find_by_id params[:id]
 	end	
+	
+	private
+		def user_params
+			params.require(:user).permit(:fname, :lname, :username, :password, :email)
+		end
 	
 end	
