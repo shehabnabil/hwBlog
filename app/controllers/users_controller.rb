64cbaeb42dd59caa8ae418da5b94927d65de7a01
Shehab Nabil <freudfloyd@gmail.com>
2015-11-11 15:55:35 +0100

    back online

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
-def index
-	@users = User.all
-end
-def new
-	@user = User.new
-end
-def show
-	user =User.find_by_id params[:id]
-
-end	
-
+	def index
+		@users = User.all
+	end
+	def new
+		@user = User.new
+	end
+	def show
+		user =User.find_by_id params[:id]
+	end	
+	def 
 end	
