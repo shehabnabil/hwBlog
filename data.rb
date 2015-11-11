874ad6cc83f2c1a7525b0aeb3cac6a76f8280b48
Shehab Nabil <freudfloyd@gmail.com>
2015-11-11 15:42:02 +0100

    removed

--- data.rb
+++ data.rb
@@ -9,3 +9,14 @@ User.create(fname: "John", lname:"Travolta", email: "Jtravolta@yahoo.com", usern
 User.create(fname: "sam", lname:"Peoples", email: "same2000@live.com", username:"salabim", password:"123456")
 
 
+Post.create(user_id: 2, title: "y kant Tori read?", body: "*snicker")
+Post.create(user_id: 1, title: "re: y kant Tori read?", body: "Bitch")
+Post.create(user_id: 3, title: "Album is dropping", body: "Don't worry, all of my lyrics still don't rhyme.")
+Post.create(user_id: 7, title: "re: Album is dropping", body: "Who needs rhyming?")
+Post.create(user_id: 2, title: "re: Ironic", body: "Yes, I know nothing in the song was actually ironic.")
+Post.create(user_id: 3, title: "re: Ironic", body: "...which in itself is actually ironic.")
+Post.create(user_id: 6, title: "Don't even know why I'm a member here.", body: "I don't sing.")
+Post.create(user_id: 4, title: "Christmas is Coming.", body: "I'm not a fan.")
+Post.create(user_id: 5, title: "re: Christmas is Coming", body: "My favorite Xmas movie is 'Treevenge'")
+Post.create(user_id: 1, title: "re: Christmas is Coming", body: "Your anti-Xmas album sucked.")
+
