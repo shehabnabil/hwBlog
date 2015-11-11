586a4a4db8d4a1af40bd35be3f0e2eeb636b67a2
Shehab Nabil <freudfloyd@gmail.com>
2015-11-11 17:23:27 +0100

    m

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+	belong_to :user
+	validates_presence_of :user_id
+	validates_presence_of :body	
+	validates_presence_of :title	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+	has_many :posts
 	validates :password, confirmation: true, presence: true, length: {minimum: 4, maximum: 15}
+	validates :email, presence: true, uniqueness: true
+	validates_presence_of :fname
+	validates_presence_of :lname
+	validates_presence_of :username
+
+
+
 end
