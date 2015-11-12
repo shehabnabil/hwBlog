a48816cf8ee7d6ca73af3db8612d8908ce733c39
Joe Spampinato <jcspampinato@gmail.com>
2015-11-12 14:01:26 -0500

    Completing Post functionality - update and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,47 @@
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
-
 	end
-
 	def new
-
+		@post = Post.new
 	end
-
 	def create
-
+		post = user_params.dup
+		post[:user_id] = session[:user_id]
+		@post = Post.new post
+		@post.save
+		redirect_to user_path(session[:user_id])
 	end
-
 	def destroy
+		post = Post.find_by_id params[:id]
 
+		if post.destroy
+		 flash[:notice] = "Post deleted successfully."
+		else
+		 flash[:alert] = "There was a problem deleting the Post."
+		end
+		redirect_to user_path(session[:user_id])
+	end
+	def edit
+		@post = Post.find_by_id params[:id]
+	end
+	def show
+		@post = Post.find params[:id]		
+	end	
+	def update
+		post = Post.find_by_id params[:id]
+		if post and post.update_attributes user_params
+			flash[:notice] = "Post updated successfully"
+			redirect_to user_path(session[:user_id])
+		else
+			flash[:alert] = "Post Updating failed."
+			redirect_to edit_post_path(params[:id])
+		end
 	end
 
-	def show
-
-		@post = Post.find params[:id]
-		
-	end	
-
-
-
+	private
+		def user_params
+			params.require(:post).permit(:title, :body, :user_id)
+		end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
 	end
 	def show
 		user =User.find_by_id params[:id]
+		@posts = user.posts
 	end	
 	
 	private
